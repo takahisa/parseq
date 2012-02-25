7517d99090d414cd3dda38a49872c7df359d7817
linerlock <x.linerlock@gmail.com>
2012-02-25 10:24:50 +0900

    signed

--- Parseq/Properties/AssemblyInfo.cs
+++ Parseq/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -11,8 +12,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: System.CLSCompliant(true)]
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("3ad5f09f-5951-49f1-afe8-f61553c2eed3")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+#pragma warning disable 1699
+#if !NOSIGN
+[assembly: AssemblyKeyFile(@"../Parseq.snk")]
+[assembly: AssemblyDelaySign(false)]
+#endif
+#pragma warning restore 1699
