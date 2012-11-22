c390ca5082f6c0a66668ceaf2575580ff2639fda
takahisa <takahisa.watanabe@outlook.com>
2012-11-22 23:29:30 +0900

    add Testings

---
+++ Parseq.Test/Combinators/CharsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parseq.Test.Combinators
+{
+    [TestClass]
+    public class CharsTest
+    {
+        
+    }
+}
