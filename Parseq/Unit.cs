721d5c3426b445540cc6ba5300c3311d53b53d83
takahisa <takahisa.watanabe@outlook.com>
2013-12-06 06:08:45 +0900

    Refine the implementation of Unit

--- Parseq/Unit.cs
+++ Parseq/Unit.cs
@@ -25,5 +25,47 @@ using System;
 
 namespace Parseq
 {
-    public enum Unit { Instance = 0 }
+    public struct Unit
+        : IComparable<Unit>, IEquatable<Unit>
+    {
+        private static readonly Unit _instance = new Unit();
+
+        public static Unit Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public static bool operator ==(Unit left, Unit right)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Unit left, Unit right)
+        {
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        int IComparable<Unit>.CompareTo(Unit other)
+        {
+            return 0;
+        }
+
+        bool IEquatable<Unit>.Equals(Unit other)
+        {
+            return true;
+        }
+    }
 }
