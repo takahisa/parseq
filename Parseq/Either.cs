89c3d385ce82d8d2f2fdbe3b4ae3d4b223c7b882
linerlock <linerlock@outlook.com>
2015-02-10 14:05:40 +0900

    Add Either.cs (revised)

---
+++ Parseq/Either.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+
+namespace Parseq
+{
+    public interface IEither<out TLeft, out TRight>
+    {
+        TResult Case<TResult>(Func<TLeft, TResult> left, Func<TRight, TResult> right);
+    }
+
+    public partial class Either
+    {
+        public static IEither<TLeft, TRight> Left<TLeft, TRight>(TLeft value)
+        {
+            return new Either.LeftImpl<TLeft, TRight>(value);
+        }
+
+        public static IEither<TLeft, TRight> Right<TLeft, TRight>(TRight value)
+        {
+            return new Either.RightImpl<TLeft, TRight>(value);
+        }
+    }
+
+    public partial class Either
+    {
+        public class LeftImpl<TLeft, TRight>
+            : IEither<TLeft, TRight>
+        {
+            public TLeft Value
+            {
+                get;
+                private set;
+            }
+
+            public LeftImpl(TLeft value)
+            {
+                this.Value = value;
+            }
+
+            public TResult Case<TResult>(Func<TLeft, TResult> left, Func<TRight, TResult> right)
+            {
+                return left(this.Value);
+            }
+        }
+
+        public class RightImpl<TLeft, TRight>
+            : IEither<TLeft, TRight>
+        {
+            public TRight Value
+            {
+                get;
+                private set;
+            }
+
+            public RightImpl(TRight value)
+            {
+                this.Value = value;
+            }
+
+            public TResult Case<TResult>(Func<TLeft, TResult> left, Func<TRight, TResult> right)
+            {
+                return right(this.Value);
+            }
+        }
+    }
+}
