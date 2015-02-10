91e5a2d98a9e0ceec31cc76deb1b01e3cea6c276
takahisa <takahisa.watanabe@outlook.com>
2015-02-10 12:29:35 +0900

    Add Pair.cs

---
+++ Parseq/Pair.Extensions.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+
+namespace Parseq
+{
+    public static partial class Pair
+    {
+        public static IPair<T0, T1> Return<T0, T1>(T0 item0, T1 item1)
+        {
+            return new Pair<T0, T1>(item0, item1);
+        }
+
+        public static TResult Case<T0, T1, TResult>(
+            this IPair<T0, T1> pair,
+                 Func<T0, T1, TResult> func)
+        {
+            return func(pair.Item0, pair.Item1);
+        }
+    }
+}
---
+++ Parseq/Pair.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+
+namespace Parseq
+{
+    public interface IPair<out T0, out T1>
+    {
+        T0 Item0
+        {
+            get;
+        }
+
+        T1 Item1
+        {
+            get;
+        }
+    }
+
+    public class Pair<T0, T1>
+        : IPair<T0, T1>
+    {
+        public T0 Item0
+        {
+            get;
+            private set;
+        }
+
+        public T1 Item1
+        {
+            get;
+            private set;
+        }
+
+        public Pair(T0 item0, T1 item1)
+        {
+            this.Item0 = item0;
+            this.Item1 = item1;
+        }
+    }
+}
