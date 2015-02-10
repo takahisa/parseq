8850431127085964a59b18d0627d145a4498e3c6
takahisa <takahisa.watanabe@outlook.com>
2015-02-10 15:36:01 +0900

    Add Seq.cs

---
+++ Parseq/Seq.Extensions.cs
@@ -0,0 +1,154 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parseq
+{
+    public static partial class Seq
+    {
+        public static void ForEach<T>(
+            this IDelayed<ISeq<T>> delayedSeq,
+                 Action<T> action)
+        {
+            foreach (var item in delayedSeq.Force().AsEnumerable())
+                action(item);
+        }
+
+        public static Boolean Any<T>(
+            this IDelayed<ISeq<T>> delayedSeq,
+                 Func<T, Boolean> predicate)
+        {
+            return delayedSeq.Force().Case(
+                empty: () => false,
+                headAndTail: pair =>
+                    predicate(pair.Item0) || Seq.Any(pair.Item1, predicate));
+        }
+
+        public static Boolean Any<T>(
+            this IDelayed<ISeq<T>> delayedSeq)
+        {
+            return delayedSeq.Force().Case(
+                empty: () => false,
+                headAndTail: _ => true);
+        }
+
+        public static Int32 Count<T>(this IDelayed<ISeq<T>> delayedSeq)
+        {
+            return delayedSeq.Force()
+                .AsEnumerable().Count();
+        }
+
+        public static T1 Foldl<T0, T1>(this IDelayed<ISeq<T0>> delayedSeq, T1 init, Func<T1, T0, T1> func)
+        {
+            return delayedSeq.Force()
+                .Case(
+                    empty: () =>
+                        init,
+                    headAndTail: pair =>
+                        Seq.Foldl(pair.Item1, func(init, pair.Item0), func));
+        }
+
+        public static T1 Foldr<T0, T1>(this IDelayed<ISeq<T0>> delayedSeq, T1 init, Func<T0, T1, T1> func)
+        {
+            return delayedSeq.Force()
+                .Case(
+                    empty: () =>
+                        init,
+                    headAndTail: pair =>
+                        func(pair.Item0, Seq.Foldr(pair.Item1, init, func)));
+        }
+
+        public static IDelayed<ISeq<T1>> Unfold<T0, T1>(T0 init, Func<T0, IOption<IPair<T1, T0>>> func)
+        {
+            return Delayed.Return(() =>
+                func(init).Case(
+                    none: () =>
+                        Seq.Empty<T1>()
+                            .Force(),
+                    some: pair =>
+                        Seq.Cons(pair.Item0, Delayed.Return(() => Seq.Unfold(pair.Item1, func).Force()))
+                            .Force()));
+        }
+
+        public static IDelayed<ISeq<T>> Singleton<T>(T value)
+        {
+            return Seq.Cons(value, Seq.Empty<T>());
+        }
+
+        public static IDelayed<ISeq<T>> Concat<T>(
+            this IDelayed<ISeq<T>> delayedSeq0,
+                 IDelayed<ISeq<T>> delayedSeq1)
+        {
+            return Delayed.SelectMany(delayedSeq0, seq0 =>
+                seq0.Case(
+                    empty: () => delayedSeq1,
+                    headAndTail: pair =>
+                        Seq.Cons(pair.Item0, Delayed.Return(() => pair.Item1.Force()))));
+        }
+
+        public static IDelayed<ISeq<T>> Where<T>(
+            this IDelayed<ISeq<T>> delayedSeq,
+                 Func<T, Boolean> predicate)
+        {
+            return Delayed.SelectMany(delayedSeq, seq =>
+                seq.Case(
+                    empty: () => Seq.Empty<T>(),
+                    headAndTail: pair => predicate(pair.Item0)
+                        ? Seq.Cons(pair.Item0, Delayed.Return(() => Seq.Where(pair.Item1, predicate).Force()))
+                        : Delayed.Return(() => Seq.Where(pair.Item1, predicate).Force())));
+        }
+
+        public static IDelayed<ISeq<T1>> Select<T0, T1>(
+            this IDelayed<ISeq<T0>> delayedSeq,
+                 Func<T0, T1> selector)
+        {
+            return Delayed.SelectMany(delayedSeq, seq =>
+                seq.Case(
+                    empty: () => 
+                        Seq.Empty<T1>(),
+                    headAndTail: pair =>
+                        Seq.Cons(selector(pair.Item0), Delayed.Return(() => Seq.Select(pair.Item1, (selector)).Force()))));
+        }
+
+        public static IDelayed<ISeq<T1>> SelectMany<T0, T1>(
+            this IDelayed<ISeq<T0>> delayedSeq,
+                 Func<T0, IDelayed<ISeq<T1>>> selector)
+        {
+            return Delayed.SelectMany(delayedSeq, seq =>
+                seq.Case(
+                    empty: () =>
+                        Seq.Empty<T1>(),
+                    headAndTail: pair =>
+                        Seq.Concat(selector(pair.Item0), Delayed.Return(() => Seq.SelectMany(pair.Item1, selector).Force()))));
+        }
+
+        public static IDelayed<ISeq<T2>> SelectMany<T0, T1, T2>(
+            this IDelayed<ISeq<T0>> delayedSeq,
+                 Func<T0, IDelayed<ISeq<T1>>> selector,
+                 Func<T0, T1, T2> projector)
+        {
+            return delayedSeq.SelectMany(value0 => selector(value0).Select(value1 => projector(value0, value1)));
+        }
+    }
+}
---
+++ Parseq/Seq.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Parseq
+{
+    public interface ISeq<T>
+    {
+        TResult Case<TResult>(
+            Func<TResult> empty,
+            Func<IPair<T, IDelayed<ISeq<T>>>, TResult> headAndTail);
+
+        IEnumerable<T> AsEnumerable();
+    }
+
+    public partial class Seq
+    {
+        public static IDelayed<ISeq<T>> Of<T>(IEnumerable<T> enumerable)
+        {
+            return Seq.Of(enumerable.GetEnumerator());
+        }
+
+        public static IDelayed<ISeq<T>> Of<T>(IEnumerator<T> enumerator)
+        {
+            return Delayed.Return(() => enumerator.MoveNext()
+                ? Seq.Cons(enumerator.Current, Delayed.Return(() => Seq.Of(enumerator).Force()))
+                    .Force()
+                : Seq.Empty<T>()
+                    .Force());
+        }
+
+        public static IDelayed<ISeq<T>> Empty<T>()
+        {
+            return Delayed.Return(SingletonClassHelper<Seq.EmptyImpl<T>>.Instance);
+        }
+
+        public static IDelayed<ISeq<T>> Cons<T>(T head, IDelayed<ISeq<T>> tail)
+        {
+            return Delayed.Return(new ConsImpl<T>(head, tail));
+        }
+    }
+
+    public partial class Seq
+    {
+        class EmptyImpl<T>
+            : ISeq<T>
+        {
+            public TResult Case<TResult>(Func<TResult> empty, Func<IPair<T, IDelayed<ISeq<T>>>, TResult> cons)
+            {
+                return empty();
+            }
+
+            public IEnumerable<T> AsEnumerable()
+            {
+                yield break;
+            }
+        }
+
+        class ConsImpl<T>
+            : ISeq<T>
+        {
+            private readonly IPair<T, IDelayed<ISeq<T>>> headAndTail;
+
+            public ConsImpl(T head, IDelayed<ISeq<T>> tail)
+            {
+                this.headAndTail = Pair.Return(head, tail);
+            }
+
+            public TResult Case<TResult>(Func<TResult> empty, Func<IPair<T, IDelayed<ISeq<T>>>, TResult> headAndTail)
+            {
+                return headAndTail(this.headAndTail);
+            }
+
+            public IEnumerable<T> AsEnumerable()
+            {
+                yield return this.headAndTail.Item0;
+
+                foreach (var item in this.headAndTail.Item1.Force().AsEnumerable())
+                    yield return item;
+            }
+        }
+    }
+}
