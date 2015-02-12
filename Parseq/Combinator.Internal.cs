e160c269c114c81b566a893e41b49676013fd7cc
linerlock <linerlock@outlook.com>
2015-02-12 20:25:05 +0900

    Add Combinator.cs (revised)

---
+++ Parseq/Combinator.Internal.cs
@@ -0,0 +1,166 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parseq
+{
+    internal static class InternalCombinator
+    {
+        public static Parser<TToken, IDelayed<ISeq<T>>> Sequence<TToken, T>(
+            IDelayed<ISeq<Parser<TToken, T>>> parsers)
+        {
+            return parsers.Force().Case(
+                    empty: () =>
+                        Parser.Return<TToken, IDelayed<ISeq<T>>>(Seq.Empty<T>()),
+                    headAndTail: pair =>
+                        pair.Item0.SelectMany(value0 => InternalCombinator.Sequence(pair.Item1).Select(value1 => Seq.Cons(value0, value1))));
+        }
+
+        public static Parser<TToken, T> Choice<TToken, T>(
+            IDelayed<ISeq<Parser<TToken, T>>> parsers)
+        {
+            return parsers.Force().Case(
+                    empty: () =>
+                        Parser.Fail<TToken, T>("Failure: Combinator.Choice"),
+                    headAndTail: pair =>
+                        pair.Item0.Or(InternalCombinator.Choice(pair.Item1)));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> Repeat<TToken, T>(
+            Parser<TToken, T> parser,
+            Int32 count)
+        {
+            return InternalCombinator.Sequence(Seq.Of(Enumerable.Repeat(parser, count)));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> Many0<TToken, T>(
+            Parser<TToken, T> parser)
+        {
+            return stream => parser(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        Reply.Success<TToken, IDelayed<ISeq<T>>>(restStream, Seq.Empty<T>()),
+                    success: (restStream, head) =>
+                        InternalCombinator.Many0(parser).Select(tail => Seq.Cons(head, tail))(restStream));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> Many1<TToken, T>(
+            Parser<TToken, T> parser)
+        {
+            return parser.SelectMany(head => InternalCombinator.Many0(parser).Select(tail => Seq.Cons(head, tail)));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> ManyTill<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> terminator)
+        {
+            return terminator.Select(_ => Seq.Empty<T>())
+                .Or(parser.SelectMany(head => InternalCombinator.ManyTill(parser, terminator)
+                    .Select(tail => Seq.Cons(head, tail))));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> SepBy0<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepBy1(parser, sep)
+                    .Or(Parser.Return<TToken, IDelayed<ISeq<T>>>(Seq.Empty<T>()));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> SepBy1<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Unit> sep)
+        {
+            return parser.SelectMany(head => InternalCombinator.Many0(sep.SelectMany(_ => parser)).Select(tail => Seq.Cons(head, tail)));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> EndBy0<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.Many0(parser.SelectMany(value => sep.Select(_ => value)));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> EndBy1<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.Many1(parser.SelectMany(value => sep.Select(_ => value)));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> SepEndBy0<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepBy0(parser, sep).SelectMany(value => sep.Optional().Select(_ => value));
+        }
+
+        public static Parser<TToken, IDelayed<ISeq<T>>> SepEndBy1<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepBy1(parser, sep).SelectMany(value => sep.Optional().Select(_ => value));
+        }
+
+        public static Parser<TToken, T> Chainl<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Func<T, T, T>> op)
+        {
+            return from lhs in parser
+                   from resultValue in InternalCombinator.ChainlRest<TToken, T>(parser, op, lhs)
+                   select resultValue;
+        }
+
+        public static Parser<TToken, T> ChainlRest<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Func<T, T, T>> op,
+            T lhs)
+        {
+            return (from func in op
+                    from rhs in parser
+                    from resultValue in InternalCombinator.ChainlRest(parser, op, func(lhs, rhs))
+                    select resultValue)
+                    .Or(Parser.Return<TToken, T>(lhs));
+        }
+
+        public static Parser<TToken, T> Chainr<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Func<T, T, T>> op)
+        {
+            return from lhs in parser
+                   from resultValue in InternalCombinator.ChainrRest<TToken, T>(parser, op, lhs)
+                   select resultValue;
+        }
+
+        public static Parser<TToken, T> ChainrRest<TToken, T>(
+            Parser<TToken, T> parser,
+            Parser<TToken, Func<T, T, T>> op,
+            T lhs)
+        {
+            return (from func in op
+                    from rhs in InternalCombinator.Chainr(parser, op)
+                    select func(lhs, rhs))
+                    .Or(Parser.Return<TToken, T>(lhs));
+        }
+    }
+}
---
+++ Parseq/Combinator.cs
@@ -0,0 +1,348 @@
+﻿/*
+ * Copyright (C) 2012 - 2015 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parseq
+{
+    public static partial class Combinator
+    {
+        public static Parser<TToken, IEnumerable<T>> Sequence<TToken, T>(
+            this IEnumerable<Parser<TToken, T>> parsers)
+        {
+            return InternalCombinator.Sequence(Seq.Of(parsers))
+                .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, T> Choice<TToken, T>(
+            this IEnumerable<Parser<TToken, T>> parsers)
+        {
+            return InternalCombinator.Choice(Seq.Of(parsers));
+        }
+
+        public static Parser<TToken, IEnumerable<T>> Repeat<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Int32 count)
+        {
+            return InternalCombinator.Repeat(parser, count)
+                    .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> Many0<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return InternalCombinator.Many0(parser)
+                    .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> Many1<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return InternalCombinator.Many1(parser)
+                    .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> ManyTill<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> terminator)
+        {
+            return InternalCombinator.ManyTill(parser, terminator)
+                .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> SepBy0<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepBy0(parser, sep)
+                    .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> SepBy1<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepBy1(parser, sep)
+                    .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> EndBy0<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.EndBy0(parser, sep)
+                .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> EndBy1<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.EndBy1(parser, sep)
+                .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> SepEndBy0<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepEndBy0(parser, sep)
+                .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+
+        public static Parser<TToken, IEnumerable<T>> SepEndBy1<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Unit> sep)
+        {
+            return InternalCombinator.SepEndBy1(parser, sep)
+                .Select(delayedSeq => delayedSeq.AsEnumerable());
+        }
+        
+        public static Parser<TToken, Unit> NotFollowedBy<TToken, T>(
+            this Parser<TToken, T> parser,
+                 String errorMessage)
+        {
+            return stream => parser.Attempt()(stream).Case(
+                    failure: (restStream, _) =>
+                        Reply.Success<TToken, Unit>(restStream, Unit.Instance),
+                    success: (reststream, _) =>
+                        Reply.Failure<TToken, Unit>(reststream, errorMessage));
+        }
+
+        public static Parser<TToken, Unit> NotFollowedBy<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return Combinator.NotFollowedBy(parser, "Failure: Combinator.NotFollowedBy");
+        }
+
+        public static Parser<TToken, T> Chainl<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Func<T, T, T>> op)
+        {
+            return InternalCombinator.Chainl(parser, op);
+        }
+
+        public static Parser<TToken, T> Chainr<TToken, T>(
+            this Parser<TToken, T> parser,
+                 Parser<TToken, Func<T, T, T>> op)
+        {
+            return InternalCombinator.Chainr(parser, op);
+        }
+
+        public static Parser<TToken, T> Between<TToken, T>(
+            this Parser<TToken, T> parser0,
+                 Parser<TToken, Unit> parser1,
+                 Parser<TToken, Unit> parser2)
+        {
+            return from value1 in parser1
+                   from value0 in parser0
+                   from value2 in parser2
+                   select value0;
+        }
+
+        public static Parser<TToken, T> Or<TToken, T>(
+            this Parser<TToken, T> parser0,
+                 Parser<TToken, T> parser1)
+        {
+            return stream => parser0(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        parser1(restStream),
+                    success: (restStream, value) =>
+                        Reply.Success<TToken, T>(restStream, value));
+        }
+
+        public static Parser<TToken, IOption<T>> Optional<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return stream => parser(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        Reply.Success<TToken, IOption<T>>(stream, Option.None<T>()),
+                    success: (restStream, value) =>
+                        Reply.Success<TToken, IOption<T>>(restStream, Option.Some<T>(value)));
+        }
+
+        public static Parser<TToken, T> Lookahead<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return stream => parser(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        Reply.Failure<TToken, T>(stream, errorMessage),
+                    success: (restStream, value) =>
+                        Reply.Success<TToken, T>(stream, value));
+        }
+
+        public static Parser<TToken, T> Attempt<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return stream => parser(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        Reply.Failure<TToken, T>(stream, errorMessage),
+                    success: (restStream, value) =>
+                        Reply.Success<TToken, T>(stream, value));
+        }
+
+        public static Parser<TToken, Unit> And<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return stream => parser(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        Reply.Failure<TToken, Unit>(stream, "Failure: Combinator.And predicate"),
+                    success: (restStream, value) =>
+                        Reply.Success<TToken, Unit>(stream, Unit.Instance));
+        }
+
+        public static Parser<TToken, Unit> Not<TToken, T>(
+            this Parser<TToken, T> parser)
+        {
+            return stream => parser(stream).Case(
+                    failure: (restStream, errorMessage) =>
+                        Reply.Success<TToken, Unit>(stream, Unit.Instance),
+                    success: (restStream, value) =>
+                        Reply.Failure<TToken, Unit>(stream, "Failure: Combinator.Not predicate"));
+        }
+
+        public static Parser<TToken, T2> Pipe<TToken, T0, T1, T2>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Func<T0, T1, T2> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   select selector(value0, value1);
+        }
+
+        public static Parser<TToken, T3> Pipe<TToken, T0, T1, T2, T3>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Parser<TToken, T2> parser2,
+                 Func<T0, T1, T2, T3> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   from value2 in parser2
+                   select selector(value0, value1, value2);
+        }
+
+        public static Parser<TToken, T4> Pipe<TToken, T0, T1, T2, T3, T4>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Parser<TToken, T2> parser2,
+                 Parser<TToken, T3> parser3,
+                 Func<T0, T1, T2, T3, T4> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   from value2 in parser2
+                   from value3 in parser3
+                   select selector(value0, value1, value2, value3);
+        }
+
+        public static Parser<TToken, T5> Pipe<TToken, T0, T1, T2, T3, T4, T5>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Parser<TToken, T2> parser2,
+                 Parser<TToken, T3> parser3,
+                 Parser<TToken, T4> parser4,
+                 Func<T0, T1, T2, T3, T4, T5> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   from value2 in parser2
+                   from value3 in parser3
+                   from value4 in parser4
+                   select selector(value0, value1, value2, value3, value4);
+        }
+
+        public static Parser<TToken, T6> Pipe<TToken, T0, T1, T2, T3, T4, T5, T6>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Parser<TToken, T2> parser2,
+                 Parser<TToken, T3> parser3,
+                 Parser<TToken, T4> parser4,
+                 Parser<TToken, T5> parser5,
+                 Func<T0, T1, T2, T3, T4, T5, T6> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   from value2 in parser2
+                   from value3 in parser3
+                   from value4 in parser4
+                   from value5 in parser5
+                   select selector(value0, value1, value2, value3, value4, value5);
+        }
+
+        public static Parser<TToken, T7> Pipe<TToken, T0, T1, T2, T3, T4, T5, T6, T7>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Parser<TToken, T2> parser2,
+                 Parser<TToken, T3> parser3,
+                 Parser<TToken, T4> parser4,
+                 Parser<TToken, T5> parser5,
+                 Parser<TToken, T6> parser6,
+                 Func<T0, T1, T2, T3, T4, T5, T6, T7> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   from value2 in parser2
+                   from value3 in parser3
+                   from value4 in parser4
+                   from value5 in parser5
+                   from value6 in parser6
+                   select selector(value0, value1, value2, value3, value4, value5, value6);
+        }
+
+        public static Parser<TToken, T8> Pipe<TToken, T0, T1, T2, T3, T4, T5, T6, T7, T8>(
+            this Parser<TToken, T0> parser0,
+                 Parser<TToken, T1> parser1,
+                 Parser<TToken, T2> parser2,
+                 Parser<TToken, T3> parser3,
+                 Parser<TToken, T4> parser4,
+                 Parser<TToken, T5> parser5,
+                 Parser<TToken, T6> parser6,
+                 Parser<TToken, T7> parser7,
+                 Func<T0, T1, T2, T3, T4, T5, T6, T7, T8> selector)
+        {
+            return from value0 in parser0
+                   from value1 in parser1
+                   from value2 in parser2
+                   from value3 in parser3
+                   from value4 in parser4
+                   from value5 in parser5
+                   from value6 in parser6
+                   from value7 in parser7
+                   select selector(value0, value1, value2, value3, value4, value5, value6, value7);
+        }
+
+        public static Parser<TToken, T> Lazy<TToken, T>(
+            Func<Parser<TToken, T>> parserFactory)
+        {
+            return Combinator.Lazy(Delayed.Return(parserFactory));
+        }
+
+        public static Parser<TToken, T> Lazy<TToken, T>(
+            IDelayed<Parser<TToken, T>> delayedParser)
+        {
+            return stream => delayedParser.Force()(stream);
+        }
+    }
+}
