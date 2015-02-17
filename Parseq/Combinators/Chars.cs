a6e502aeba37797983d9e3d8e72c84d9f65f7e6a
takahisa <takahisa.watanabe@outlook.com>
2015-02-17 16:20:40 +0900

    Add some utility combinators

--- Parseq/Combinators/Chars.cs
+++ Parseq/Combinators/Chars.cs
@@ -121,6 +121,19 @@ namespace Parseq.Combinators
             return Chars.Char(System.Char.IsWhiteSpace);
         }
 
+        public static Parser<Char, Char> Oct()
+        {
+            return Chars.OneOf('0', '1', '2', '3', '4', '5', '6', '7');
+        }
+
+        public static Parser<Char, Char> Hex()
+        {
+            return Chars.OneOf(
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f',
+                'A', 'B', 'C', 'D', 'E', 'F');
+        }
+
         public static Parser<Char, Char> Satisfy(Char c)
         {
             return Chars.Char(c);
--- Parseq/Parser.Extensions.cs
+++ Parseq/Parser.Extensions.cs
@@ -20,6 +20,8 @@
  * 
  */
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace Parseq
